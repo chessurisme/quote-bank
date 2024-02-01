18b6956e33aadc174772cf7ff5aca90b75d15bb9
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-02-01 22:18:44 +0800

    Add JSDoc to the Quote class

--- src/js/components/quote.js
+++ src/js/components/quote.js
@@ -1,11 +1,23 @@
 'use strict'
 
+/**
+ * Represents a quote with an ID and text.
+ */
 class Quote {
+  /**
+   * Initializes a new instance of the Quote class with the given ID and text.
+   * @param {number} id - The ID of the quote.
+   * @param {string} text - The text of the quote.
+   */
   constructor(id, text) {
     this.id = id
     this.text = text
   }
 
+  /**
+   * Updates the text of the quote with the given new text.
+   * @param {string} newText - The new text for the quote.
+   */
   editText(newText) {
     this.text = newText
   }
