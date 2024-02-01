b0b5d4e19e54d7048c9d7834a2bf24cc2a79a3ce
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-02-01 22:27:16 +0800

    Add JSDoc to the Quote Bank class

--- src/js/components/quoteBank.js
+++ src/js/components/quoteBank.js
@@ -1,13 +1,32 @@
 'use strict'
 
+/**
+ * Represents a collection of quotes.
+ */
 class QuoteBank {
-  constructor(){
+  /**
+   * Initializes a new instance of the QuoteBank class.
+   */
+  constructor() {
+    /**
+     * An array that stores the quotes in the collection.
+     * @type {Array}
+     */
     this.quotes = []
   }
 
+  /**
+   * Adds a quote to the collection.
+   * @param {Object} quote - The quote to be added.
+   */
   addQuote(quote) {
     this.quotes.push(quote)
   }
+
+  /**
+   * Deletes a quote from the collection based on its ID.
+   * @param {number} id - The ID of the quote to be deleted.
+   */
   deleteQuote(id) {
     this.quotes = this.quotes.filter(quote => quote.id !== id)
   }
